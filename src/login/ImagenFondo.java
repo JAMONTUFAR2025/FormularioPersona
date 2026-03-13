9d66eda02c0c91af52f9f06d55e9e6a85c82cbb0
ACME_DataScraping <https://github.com/JAMONTUFAR2025/ACME_DataScrapping.git>
2026-03-12 22:23:52 -0600

    Trabajo del [2026-03-12]

---
+++ src/login/ImagenFondo.java
@@ -0,0 +1,29 @@
+package login;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aleja
+ */
+public class ImagenFondo extends javax.swing.JPanel {
+    private java.awt.Image imagen;
+
+    @Override
+    public void paint(java.awt.Graphics g) {
+        // Carga la imagen desde la ruta de tu proyecto
+        imagen = new javax.swing.ImageIcon(getClass().getResource("../assets/fondo.jpg")).getImage();
+        
+        // Dibuja la imagen para que ocupe todo el panel
+        g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+        
+        // Hace que el panel sea transparente para ver los componentes
+        setOpaque(false);
+        super.paint(g);
+    }
+}
